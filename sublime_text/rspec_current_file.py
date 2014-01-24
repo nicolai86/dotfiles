8a7667a2db019957b011aebf9c0d097e9e9ecf20
Raphael Randschau <nicolai86@me.com>
2014-01-24 08:21:16 +0100

    add rspec_current_line_in_file window command

--- sublime_text/rspec_current_file.py
+++ sublime_text/rspec_current_file.py
@@ -3,19 +3,37 @@ import subprocess
 from os.path import expanduser
 
 class RspecCurrentFileCommand(sublime_plugin.WindowCommand):
-  def run(self):
+  def spec_path(self):
     self.window.run_command("copy_relative_path")
-    spec_path = sublime.get_clipboard()
+    return sublime.get_clipboard()
 
-    if not spec_path:
-      print("No file open. Ignoring")
-      return
-
+  def execute_spec(self, spec_path, path_suffix):
     if "spec" in spec_path:
       print("Looks like a spec. Running rspec")
       home = expanduser("~")
-      spec_command = ['/usr/bin/osascript', home + '/.dotfiles/osascript/write_iterm2.scpt', "rspec " + spec_path]
+      spec_command = ['/usr/bin/osascript', home + '/.dotfiles/osascript/write_iterm2.scpt', "rspec " + spec_path + path_suffix]
       proc = subprocess.Popen(spec_command, stdout = subprocess.PIPE)
       print(proc.communicate())
     else:
       print("No spec file. Ignoring.")
+
+  def run(self):
+    spec_path = self.spec_path()
+    if not spec_path:
+      print("No file open. Ignoring")
+      return
+    self.execute_spec(spec_path, "")
+
+class RspecCurrentLineInFile(RspecCurrentFileCommand):
+  def run(self):
+    spec_path = self.spec_path()
+    if not spec_path:
+      print("No file open. Ignoring")
+      return
+
+    active_view = self.window.active_view()
+    first_selection = active_view.sel()[0]
+    (row,col) = active_view.rowcol(first_selection.begin())
+    line = row + 1
+
+    self.execute_spec(spec_path, ":" + str(line))
