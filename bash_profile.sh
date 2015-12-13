55aaebf7a3b2a090fdb3139784ac6a63dec4e29f
Raphael Randschau <nicolai86@me.com>
2015-12-13 16:46:09 +0100

    Add install.sh to setup symlinks

--- bash_profile.sh
+++ bash_profile.sh
@@ -7,7 +7,6 @@ fi
 export EDITOR=subl
 export VISUAL=vim
 
-
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -nfs $HOME/.dotfiles/bash_profile.sh $HOME/.bash_profile
+ln -nfs $HOME/.dotfiles/editorconfig $HOME/.editorconfig
+ln -nfs $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+ln -nfs $HOME/.dotfiles/git/gitignore $HOME/.gitignore
+ln -nfs $HOME/.dotfiles/git/gitmessage.txt $HOME/.gitmessage.txt
+ln -nfs $HOME/.dotfiles/psql/psqlrc $HOME/.psqlrc
+ln -nfs $HOME/.dotfiles/tigrc $HOME/.tigrc
+ln -nfs $HOME/.dotfiles/vimrc $HOME/.vimrc
