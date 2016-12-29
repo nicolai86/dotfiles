02614ffe68bc244edb420ed5c4a687073c9d1900
Raphael Randschau <nicolai86@me.com>
2016-12-29 12:17:52 +0100

    add irssi + otr docker container

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,3 +7,6 @@ alias redc="red be rails c"
 alias t="theca"
 # git aliases
 alias git_week="git log --since '7 day ago' --no-merges --author 'Raphael Randschau'"
+
+# docker shorthands
+alias irssi="docker run --rm -it --name irssi -e TERM -u $(id -u):$(id -g) --log-driver=none -v $HOME/.irssi:/home/user/.irssi irssi-otr:v1.0.2"
