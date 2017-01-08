6aba436ad09418ee442b9234ab37ec7d28de0356
Raphael Randschau <nicolai86@me.com>
2017-01-08 18:30:55 +0100

    chore/traq: add comments regarding s3 sync

--- bash/traq.sh
+++ bash/traq.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export TRAQ_PATH=$GOPATH/src/github.com/nicolai86/traq
 export TRAQ_DATA_DIR="$HOME/Library/traq"
+
+# to upload all: aws s3 sync ~/Library/traq/ s3://nicolai86-traq-data
+# to download: aws s3 sync s3://nicolai86-traq-data ~/Library/traq/
