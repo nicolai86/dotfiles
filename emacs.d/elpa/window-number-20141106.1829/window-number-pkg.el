0f8166bce73aa64f6913ece232d14ffdcc4e7194
Raphael Randschau <nicolai86@me.com>
2017-03-05 16:12:49 +0100

    feat(work): add emacs setup

---
+++ emacs.d/elpa/window-number-20141106.1829/window-number-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "window-number" "20141106.1829" "Select windows by numbers." 'nil :url "https://github.com/nikolas/window-number" :keywords '("windows"))
