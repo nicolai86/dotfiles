604726c827b7c3d5fae59adb7b56f86a5885751b
Raphael Randschau <nicolai86@me.com>
2019-03-25 22:52:13 -0700

    drop emacs.

--- emacs.d/elpa/window-number-20141106.1829/window-number-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "window-number" "20141106.1829" "Select windows by numbers." 'nil :url "https://github.com/nikolas/window-number" :keywords '("windows"))
